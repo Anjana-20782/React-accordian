33a7c2489f12eca368aa0f7ab9c8e541c36dbe9d
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-22 17:20:12 +0530

    new contents added in the accordian

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Accordion from "./components/Accordion";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Accordion Example</h1>
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
