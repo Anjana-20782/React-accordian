b9061cabe3efa11fb7c327f697e64cc649179228
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-25 23:09:21 +0530

    new centents added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,2 @@
 import Accordion from "./components/Accordion";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React Accordion Example</h1>
-      <Accordion />
-    </div>
-  );
-}
-
-export default App;
